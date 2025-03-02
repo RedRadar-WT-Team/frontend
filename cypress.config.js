c93a439a804b321bf3561db056f45c668a18333d
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-02 15:04:03 -0500

    Fixed it?

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-<<<<<<< HEAD
     baseUrl: "http://localhost:5173",
 
     setupNodeEvents(on, config) {
@@ -10,10 +9,4 @@ export default defineConfig({
       return config;
     },
    }
-=======
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
->>>>>>> 5b4209fdf8cdd12e8064412aa3ee65320476620c
 });
