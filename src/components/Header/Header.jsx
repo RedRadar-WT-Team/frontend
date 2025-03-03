d3e832c6464e9bdc1498b82eba0b2c2d1dcef220
Kevin Newland <kevin.newland@yahoo.com>
2025-03-02 22:11:37 -0600

    moved popout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,12 @@
 // Header.js
-import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import Logo from '/src/assets/logo.webp';
 import BurgerMenu from '/src/assets/burger_menu.svg';
 import './Header.css';
 
-function Header() {
-  const [isOpen, setIsOpen] = useState(false)
-
-  function popOutMenu() {
-    setIsOpen(!isOpen);
-  }
-
+function Header({popOutMenu}) {
   return (
+    <main className='header-container'>
       <header>
         <Link to="/">
           <img className='logo' src={Logo} alt="repRadar logo" />
@@ -24,27 +18,9 @@ function Header() {
           <button className='burger-button'>
             <img className='burger-img' src={BurgerMenu} alt="Menu" onClick={popOutMenu}></img>
           </button>
-
-          {isOpen && (
-            <div className={`pop-up ${isOpen ? 'open' : ''}`}>
-              <ul>
-                <li>
-                  <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/executive-orders">All Executive Orders</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/profile">User Profile</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/logout">Logout</NavLink>
-                </li>
-              </ul>
-            </div>
-          )}
         </section>
       </header>
+    </main>
   );
 }
 
