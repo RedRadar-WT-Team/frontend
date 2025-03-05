79d7dfb823acbf6d8677ad140275b69b581eae60
elysableu <elysabward@gmail.com>
2025-03-04 17:53:04 -0700

    Update header css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 // Header.js
 import { Link, NavLink } from 'react-router-dom';
-import Logo from '/src/assets/logo.webp';
+import Logo from '/src/assets/logo_transparent_bg.svg';
 import BurgerMenu from '/src/assets/burger_menu.svg';
 import './Header.css';
 
-function Header({popOutMenu}) {
+function Header({popOutMenu, isOpen}) {
   return (
     <main className='header-container'>
       <header>
@@ -15,9 +15,11 @@ function Header({popOutMenu}) {
         <h1>RepRadar</h1>
           
         <section className='pop-out-container'>
-          <button className='burger-button'>
+          <div className={`${isOpen ? 'slope' : ''}`}></div>
+          <button className={`burger-button ${isOpen ? 'open' : ''}`}>
             <img className='burger-img' src={BurgerMenu} alt="Menu" onClick={popOutMenu}></img>
           </button>
+          <div className={`${isOpen ? 'slope' : ''}`}></div>
         </section>
       </header>
     </main>
