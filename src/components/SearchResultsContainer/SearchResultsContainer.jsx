bf1d3f02281e16439cadce066cc181698b644cf1
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-03-05 13:04:28 -0800

    add:dummyRepData

---
+++ src/components/SearchResultsContainer/SearchResultsContainer.jsx
@@ -0,0 +1,27 @@
+import './SearchResultsContainer.css';
+import SearchResultsCard from '../SearchResultsCard/SearchResultsCard.jsx';
+
+function SearchResultCards( { repData } ) {
+  console.log("RepData:", repData)
+  const repDataCards = repData.map(rep => {
+    return (
+      < SearchResultsCard 
+          name  = { rep.name } 
+          area  = { rep.area }
+          party = { rep.party } 
+          state = { rep.state }
+          id    = { rep.id } 
+          key   = { rep.id }
+      />
+    )
+  })
+
+  return (
+      <section className='MoviesContainer'>
+        { repDataCards }
+      </section>
+  );
+}
+
+export default SearchResultCards
+
