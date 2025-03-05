e7acc199a93e6516d5b9718420676c18278ddd76
Patrick Shim <patrick.shim91@yahoo.com>
2025-03-05 18:26:03 -0500

    Updated SearchResultCards props to rep.attributes

--- src/components/SearchResultsContainer/SearchResultsContainer.jsx
+++ src/components/SearchResultsContainer/SearchResultsContainer.jsx
@@ -1,17 +1,17 @@
 import './SearchResultsContainer.css';
 import SearchResultsCard from '../SearchResultsCard/SearchResultsCard.jsx';
 
-function SearchResultCards( { repData } ) {
-  console.log("RepData:", repData)
-  const repDataCards = repData.map(rep => {
+function SearchResultCards( { reps } ) {
+  const repDataCards = reps.data.map((rep) => {
     return (
       < SearchResultsCard 
-          name  = { rep.name } 
-          area  = { rep.area }
-          party = { rep.party } 
-          state = { rep.state }
-          id    = { rep.id } 
-          key   = { rep.id }
+          photoURL = { rep.attributes.photo_url }
+          name= { rep.attributes.name } 
+          area= { rep.attributes.area }
+          party= { rep.attributes.party } 
+          state= { rep.attributes.state }
+          id= { rep.attributes.id } 
+          key = { rep.attributes.id }
       />
     )
   })
