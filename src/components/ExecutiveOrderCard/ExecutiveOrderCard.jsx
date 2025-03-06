6b9639b3e6073ea5d81cdc3f2eada39735f93639
Kristin Weiland <kristinweiland@gmail.com>
2025-03-06 11:46:07 -0800

    Adjust data access.

--- src/components/ExecutiveOrderCard/ExecutiveOrderCard.jsx
+++ src/components/ExecutiveOrderCard/ExecutiveOrderCard.jsx
@@ -6,7 +6,7 @@ function ExecutiveOrderCard({ execOrder }) {
   console.log("execOrder: ", execOrder)
   return (
     <section className='ExecutiveOrderCard'>
-      <h2>{execOrder.attributes.title}</h2>
+      <h2>{execOrder.title}</h2>
       <h3>{execOrder.executive_order_number}</h3>
       <p>{execOrder.publication_date}</p>
       <a href={execOrder.html_url}>Full Executive Order</a>
