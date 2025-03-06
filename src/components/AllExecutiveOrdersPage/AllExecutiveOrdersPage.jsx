d943635a12c3806da9b07702c08db803e98b6d88
Kristin Weiland <kristinweiland@gmail.com>
2025-03-06 11:44:19 -0800

    Add .data to access the data we need to map over for allExecOrders.

--- src/components/AllExecutiveOrdersPage/AllExecutiveOrdersPage.jsx
+++ src/components/AllExecutiveOrdersPage/AllExecutiveOrdersPage.jsx
@@ -1,11 +1,14 @@
 import './AllExecutiveOrdersPage.css';
 import ExecutiveOrderCard from '../ExecutiveOrderCard/ExecutiveOrderCard'
+import MenuPopUp from "../MenuPopUp/MenuPopUp";
 // import { data } from 'react-router-dom';
 
 function AllExecutiveOrdersPage({allExecutiveOrders}) {
   console.log("ALL ExecOrders: ", allExecutiveOrders)
   console.log("first ExecOrder: ", allExecutiveOrders)
-  const allExecOrders = allExecutiveOrders.map((execOrder) => {
+  console.log("Type of allExecutiveOrders.data:", typeof allExecutiveOrders.data);
+
+  const allExecOrders = allExecutiveOrders.data.map((execOrder) => {
       return (
       <ExecutiveOrderCard
         key={execOrder.id}
