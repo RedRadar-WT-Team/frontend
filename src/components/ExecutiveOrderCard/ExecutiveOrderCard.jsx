cf88e9449ce2eedff496b6f953da1ce80a2f9670
elysableu <elysabward@gmail.com>
2025-03-06 15:30:19 -0700

    Update ExecutiveOrderCards to sne details up to app and redirect to detailspage

--- src/components/ExecutiveOrderCard/ExecutiveOrderCard.jsx
+++ src/components/ExecutiveOrderCard/ExecutiveOrderCard.jsx
@@ -1,15 +1,27 @@
+import SearchCard from '../SearchResultsCard/SearchResultsCard';
 import './ExecutiveOrderCard.css';
 import { Link } from 'react-router-dom';
 
 
-function ExecutiveOrderCard({ execOrder }) {
-  console.log("execOrder: ", execOrder)
+function ExecutiveOrderCard({ execOrder, setDetailsTarget, getDetails }) {
+  
+  const cardClick = () => {
+    setDetailsTarget("EO")
+    console.log(execOrder)
+    getDetails(execOrder.document_number)
+  }
+
+  const starClick = (event) => {
+    event.stopPropagation();
+    alert("star clicked!")
+  }
+
   return (
-    <section className='ExecutiveOrderCard'>
+    <section className='ExecutiveOrderCard' onClick={cardClick}>
       <h2>{execOrder.title}</h2>
-      <h3>{execOrder.executive_order_number}</h3>
+      <h3>{execOrder.document_number}</h3>
       <p>{execOrder.publication_date}</p>
-      <a href={execOrder.html_url}>Full Executive Order</a>
+      <button onClick={starClick}><img className="star" src="src/assets/Five-pointed_star.svg" /></button>
     </section>
   );
 }
