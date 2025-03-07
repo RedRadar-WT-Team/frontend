1209e4435b45d04bc4d600dde44867c6101deff2
Kevin Newland <kevin.newland@yahoo.com>
2025-03-07 13:36:17 -0600

    created cypress tests for EO details page

---
+++ cypress/e2e/eo_detail_page_spec.cy.js
@@ -0,0 +1,33 @@
+describe('Eos Detail Page', () => {
+  beforeEach(() => {
+    cy.visit('/'); 
+    cy.get('.burger-button').click()
+    cy.get('.active').click()
+    cy.get('.ExecutiveOrderCard').first().click()
+  });
+
+  it('displays title on page load', () => {
+    cy.get('h1')
+    .contains('RepRadar')
+  })
+
+  it('displays title of EO', () => {
+    cy.get('.EODetails')
+    cy.get('h1').should('contain', "Further Amendment to Duties Addressing the Synthetic Opioid Supply Chain in the People's Republic of China")
+  })
+
+  it('displays the document number of EO', () => {
+    cy.get('h2')
+    .should('contain', "2025-03775")
+  })
+
+  it('displays the publication date of EO', () => {
+    cy.get('h2')
+    .should('contain', "March 07, 2025")
+  })
+
+  it('displays the link to the full EO', () => {
+    cy.get('a')
+    .should('have.attr', 'href')
+  })
+})
